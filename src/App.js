df850a05f22701317499c0dadfc8ef44492d0634
AldSenior <aldsio196@gmail.com>
2024-10-27 09:11:22 +0300

    verse-1.1

--- src/App.js
+++ src/App.js
@@ -20,17 +20,64 @@ function App() {
     return () => clearInterval(Time)
   }, [])
 
+
+  const handlePlayPauseAudio = () => {
+    if (musRef.current) {
+      if (!isStartSong) {
+        setIsStartSong(true)
+        musRef.current.play()
+      }
+      else {
+        setIsStartSong(false)
+        musRef.current.pause()
+      }
+    }
+  }
+
+  const handleChangeNextAudio = () => {
+    if (currMusicIndex == MUSICS.name.length - 1) {
+      setCurrMusicIndex(0)
+    } else {
+      setCurrMusicIndex(cur => cur + 1)
+    }
+  }
+
+  const handleChangePrevAudio = () => {
+    if (currMusicIndex == 0) {
+      setCurrMusicIndex(MUSICS.name.length - 1)
+    } else {
+      setCurrMusicIndex(cur => cur - 1)
+    }
+  }
+
+  const handleVolumeChange = (e) => {
+    const newVolume = parseFloat(e.target.value)
+    if (musRef.current) {
+      musRef.current.volume = newVolume // Изменится громкость аудио
+    }
+  }
+  const handleCurTimeChange = (e) => { // Изменится текущее время музыки
+    const newTime = parseFloat(e.target.value)
+    if (musRef.current) {
+      musRef.current.currentTime = newTime
+    }
+  }
+  const onEndedAudio = () => { // При окончании музыки начинаеться следующая
+    setCurrMusicIndex(prev => prev + 1)
+  }
+
+  const isLoadedMetaData = () => { // если при переключении трека предыдущий играл, то следующий тоже начинает сразу играть
+    if (isStartSong) {
+      musRef.current.play()
+    }
+  }
+
   return (
     <div className="App">
       <audio ref={musRef}
-        onEnded={() => {
-          setCurrMusicIndex(prev => prev + 1)
-        }}
-        onLoadedMetadata={() => {
-          if (isStartSong) {
-            musRef.current.play()
-          }
-        }} src={`/Music/REJDI_-_${MUSICS.name[currMusicIndex]}.mp3`}></audio>
+        onEnded={onEndedAudio}
+        onLoadedMetadata={
+          isLoadedMetaData} src={`/Music/REJDI_-_${MUSICS.name[currMusicIndex]}.mp3`}></audio>
       <main>
 
       </main>
@@ -38,54 +85,24 @@ function App() {
         <p>{MUSICS.name[currMusicIndex]}</p>
         <div className="play-menu-music">
           <div className='play-menu-buttons'>
-            <div onClick={() => {
-              if (currMusicIndex == 0) {
-                setCurrMusicIndex(MUSICS.name.length - 1)
-
-              } else {
-                setCurrMusicIndex(cur => cur - 1)
-              }
-            }
-            }><MdSkipPrevious /></div>
-            <div onClick={() => {
-              if (musRef.current) {
-                if (!isStartSong) {
-                  setIsStartSong(true)
-                  musRef.current.play()
-                }
-                else {
-                  setIsStartSong(false)
-                  musRef.current.pause()
-                }
-              }
-            }}>{isStartSong ? <FaPause /> : <FaPlay />}</div>
-            <div onClick={() => {
-              if (currMusicIndex == MUSICS.name.length - 1) {
-                setCurrMusicIndex(0)
-              } else {
-                setCurrMusicIndex(cur => cur + 1)
-              }
-            }
+            <div onClick={handleChangePrevAudio}>
+              <MdSkipPrevious />
+            </div>
+            <div onClick={handlePlayPauseAudio}>
+              {isStartSong ? <FaPause /> : <FaPlay />}
+            </div>
+            <div onClick={
+              handleChangeNextAudio
             }><MdSkipNext /></div>
           </div>
           <div className='menu-play-line'>
-            <input type="range" min={0} max={musRef.current.duration} value={musRef.current.currentTime} onChange={(e) => {
-              const newTime = parseFloat(e.target.value)
-              if (musRef.current) {
-                musRef.current.currentTime = newTime
-              }
-            }} />
+            <input type="range" min={0} max={musRef.current.duration} value={musRef.current.currentTime} onChange={handleCurTimeChange} />
             <p>{moment.utc(duration * 1000).format("mm:ss")}</p>
           </div>
         </div>
         <div className='setting_music'>
           <h3>Громкость</h3>
-          <input type="range" className='volume' value={musRef.current?.volume || 0} min={0} max={1} step={0.01} onChange={(e) => {
-            const newVolume = parseFloat(e.target.value)
-            if (musRef.current) {
-              musRef.current.volume = newVolume // Изменится громкость аудио
-            }
-          }} />
+          <input type="range" className='volume' value={musRef.current?.volume || 0} min={0} max={1} step={0.01} onChange={handleVolumeChange} />
         </div>
       </div>
     </div >
